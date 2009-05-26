a41346c08f9b1faab56ee5e1bcf8aa55a8653987
kla <la.kien@gmail.com>
2009-05-25 22:59:47 -0400

    re-wrote and cleaned up model callback tests

--- lib/php-activerecord/test/models/VenueCB.php
+++ lib/php-activerecord/test/models/VenueCB.php
@@ -3,107 +3,18 @@ class VenueCB extends ActiveRecord\Model
 {
 	static $table_name = 'venues';
 
-	static $after_construct;
-	static $before_save;
-	static $after_save;
-	static $before_create;
-	static $after_create;
-	static $before_update;
-	static $after_update;
-	static $before_validation;
-	static $after_validation;
-	static $before_validation_on_create;
-	static $after_validation_on_create;
-	static $before_validation_on_update;
-	static $after_validation_on_update;
-	static $before_destroy;
-	static $after_destroy;
+	static $after_destroy = array('after_destroy_one', 'after_destroy_two');
+	static $before_destroy = 'before_destroy_using_string';
 
-	public function test_after_construct()
-	{
-		CallBackTest::instance()->run_tests($this);
-	}
+	// DO NOT add a static $after_construct for this. we are testing
+	// auto registration of callback with this
+	public function after_construct() {}
 
-	public function test_before_validation()
-	{
-		CallBackTest::instance()->run_tests($this);
-	}
+	public function non_generic_after_construct() {}
 
-	public function test_before_validation_on_create()
-	{
-		CallBackTest::instance()->run_tests($this);
-	}
+	public function after_destroy_one() {}
+	public function after_destroy_two() {}
 
-	public function test_before_validation_on_update()
-	{
-		CallBackTest::instance()->run_tests($this);
-	}
-
-	public function test_before_validation_returns_false_cancels_call_backs()
-	{
-		CallBackTest::instance()->run_tests($this);
-		return false;
-	}
-
-	public function test_after_validation()
-	{
-		CallBackTest::instance()->run_tests($this);
-	}
-
-	public function test_after_validation_on_create()
-	{
-		CallBackTest::instance()->run_tests($this);
-	}
-
-	public function test_after_validation_on_update()
-	{
-		CallBackTest::instance()->run_tests($this);
-	}
-
-	public function test_before_save()
-	{
-		CallBackTest::instance()->run_tests($this);
-	}
-
-	public function test_before_update()
-	{
-		CallBackTest::instance()->run_tests($this);
-	}
-
-	public function test_after_save()
-	{
-		CallBackTest::instance()->run_tests($this);
-	}
-
-	public function test_after_update()
-	{
-		CallBackTest::instance()->run_tests($this);
-	}
-
-	public function test_before_create()
-	{
-		CallBackTest::instance()->run_tests($this);
-	}
-
-	public function test_after_create()
-	{
-		CallBackTest::instance()->run_tests($this);
-	}
-
-	public function test_before_save_returns_false_cancels_call_backs()
-	{
-		CallBackTest::instance()->run_tests($this);
-		return false;
-	}
-
-	public function test_before_destroy()
-	{
-		CallBackTest::instance()->run_tests($this);
-	}
-
-	public function test_after_destroy()
-	{
-		CallBackTest::instance()->run_tests($this);
-	}
+	public function before_destroy_using_string() {}
 }
 ?>
