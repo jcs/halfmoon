31a07da6c781561ae68d34f7a7539e97cb85a550
joshua stein <jcs@jcs.org>
2010-01-25 13:09:04 -0700

    if the .phtml file doesn't exist, try a .pxml file and set the content type accordingly

--- lib/controller.php
+++ lib/controller.php
@@ -115,7 +115,13 @@ class ApplicationController {
 			if (file_exists($full_file = HALFMOON_ROOT . "/views/"
 			. $tf . ".phtml"))
 				$this->_really_render_file($full_file, $vars);
-			else
+			elseif (file_exists($full_file = HALFMOON_ROOT . "/views/"
+			. $tf . ".pxml")) {
+				/* TODO: check for an existing content-type already set by the
+				 * user */
+				header("Content-type: application/xml");
+				$this->_really_render_file($full_file, $vars);
+			} else
 				throw new RenderException("no template file " . $full_file);
 		}
 
