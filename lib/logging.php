ca36811fb672979ef423ecb9c5b7cae3464f3542
joshua stein <jcs@jcs.org>
2010-03-12 18:52:54 -0700

    apparently some bad behavior of apache puts stderr contents in the global errorlog and not in the virtualhost's errorlog, so go back to using error_log for now

--- lib/logging.php
+++ lib/logging.php
@@ -10,13 +10,8 @@ class Log {
 		error_log($string);
 	}
 
-	/* by directly writing to stderr, we avoid prefixing lines with [error]
-	 * like error_log does */
 	static function info($string) {
-		$stderr = fopen("php://stderr", "w");
-		/* match apache's logging style */
-		fwrite($stderr, date("[D M d H:i:s Y] ") . $string . "\n");
-		fclose($stderr);
+		error_log($string);
 	}
 
 	static function warn($string) {
