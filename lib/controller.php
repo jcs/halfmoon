7bd96d4fb4a837c58501e779d51223579f7f395a
joshua stein <jcs@jcs.org>
2010-03-15 20:45:15 -0700

    link_from_obj_or_string is now in HTMLHelper

--- lib/controller.php
+++ lib/controller.php
@@ -86,7 +86,7 @@ class ApplicationController {
 
 	/* cancel all buffered output, send a location: header, and exit */
 	public function redirect_to($obj_or_url) {
-		$link = link_from_obj_or_string($obj_or_url);
+		$link = HTMLHelper::link_from_obj_or_string($obj_or_url);
 
 		/* prevent any content from getting to the user */
 		while (@ob_end_clean())
