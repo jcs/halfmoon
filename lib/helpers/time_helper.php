eaf133057175986d3dfaed885f320880fc4097ac
joshua stein <jcs@jcs.org>
2010-03-15 20:28:50 -0700

    fix namespace issues

--- lib/helpers/time_helper.php
+++ lib/helpers/time_helper.php
@@ -19,48 +19,48 @@ class TimeHelper extends Helper {
 			intval($from_time->format("U"));
 
 		$distance_in_minutes = round(abs($seconds_diff / 60));
-		$distance_in_seconds = round(abs($secons_diff));
+		$distance_in_seconds = round(abs($seconds_diff));
 
-		if (HalfMoon\Utils::is_or_between($distance_in_minutes, array(0, 1))) {
+		if (Utils::is_or_between($distance_in_minutes, array(0, 1))) {
 			if (!$include_seconds)
 				return ($distance_in_minutes == 0 ? "less than 1 minute" :
 					$distance_in_minutes . " minute"
 					. ($distance_in_minutes == 1 ? "" : "s"));
 
-			if (HalfMoon\Utils::is_or_between($distance_in_seconds, array(0, 4)))
+			if (Utils::is_or_between($distance_in_seconds, array(0, 4)))
 				return "less than 5 seconds";
-			elseif (HalfMoon\Utils::is_or_between($distance_in_seconds, array(5, 9)))
+			elseif (Utils::is_or_between($distance_in_seconds, array(5, 9)))
 				return "less than 10 seconds";
-			elseif (HalfMoon\Utils::is_or_between($distance_in_seconds, array(10, 19)))
+			elseif (Utils::is_or_between($distance_in_seconds, array(10, 19)))
 				return "less than 20 seconds";
-			elseif (HalfMoon\Utils::is_or_between($distance_in_seconds, array(20, 39)))
+			elseif (Utils::is_or_between($distance_in_seconds, array(20, 39)))
 				return "less than half a minute";
 			else
 				return "1 minute";
 		}
 
-		elseif (HalfMoon\Utils::is_or_between($distance_in_minutes, array(2, 44)))
+		elseif (Utils::is_or_between($distance_in_minutes, array(2, 44)))
 			return $distance_in_minutes . " minutes";
 
-		elseif (HalfMoon\Utils::is_or_between($distance_in_minutes, array(45, 89)))
+		elseif (Utils::is_or_between($distance_in_minutes, array(45, 89)))
 			return "about 1 hour";
 
-		elseif (HalfMoon\Utils::is_or_between($distance_in_minutes, array(90, 1439)))
+		elseif (Utils::is_or_between($distance_in_minutes, array(90, 1439)))
 			return "about " . round($distance_in_minutes / 60) . " hours";
 
-		elseif (HalfMoon\Utils::is_or_between($distance_in_minutes, array(1440, 2879)))
+		elseif (Utils::is_or_between($distance_in_minutes, array(1440, 2879)))
 			return "about 1 day";
 
-		elseif (HalfMoon\Utils::is_or_between($distance_in_minutes, array(2880, 43199)))
+		elseif (Utils::is_or_between($distance_in_minutes, array(2880, 43199)))
 			return "about " . round($distance_in_minutes / 1440) . " days";
 
-		elseif (HalfMoon\Utils::is_or_between($distance_in_minutes, array(43200, 86399)))
+		elseif (Utils::is_or_between($distance_in_minutes, array(43200, 86399)))
 			return "about 1 month";
 
-		elseif (HalfMoon\Utils::is_or_between($distance_in_minutes, array(86400, 525599)))
+		elseif (Utils::is_or_between($distance_in_minutes, array(86400, 525599)))
 			return "about " . round($distance_in_minutes / 43200) . " months";
 
-		elseif (HalfMoon\Utils::is_or_between($distance_in_minutes, array(525600, 1051199)))
+		elseif (Utils::is_or_between($distance_in_minutes, array(525600, 1051199)))
 			return "about 1 year";
 
 		else
@@ -69,14 +69,14 @@ class TimeHelper extends Helper {
 
 	/* like distance_of_time_in_words, but where to_time is fixed to now */
 	public function time_ago_in_words($from_time, $include_seconds = false) {
-		$now = new DateTime("now");
+		$now = new \DateTime("now");
 
 		if (get_class($from_time) == "DateTime")
 			$now->setTimezone($from_time->getTimezone());
 		elseif (is_int($from_time))
-			$from_time = new DateTime($from_time);
+			$from_time = new \DateTime($from_time);
 
-		return $this->distance_of_time_in_words($from_time, $now,
+		return TimeHelper::distance_of_time_in_words($from_time, $now,
 			$include_seconds);
 	}
 }
