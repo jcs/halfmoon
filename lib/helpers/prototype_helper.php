1612f7c2869087d08130a3af886588c74dc7d315
joshua stein <jcs@jcs.org>
2010-08-16 22:13:53 -0500

    new $prototype helper to deal with ajax and javascript stuff

---
+++ lib/helpers/prototype_helper.php
@@ -0,0 +1,159 @@
+<?php
+/*
+	prototype javascript helpers
+*/
+
+namespace HalfMoon;
+
+class PrototypeHelper extends FormHelper {
+	public function form_remote_tag($options = array(),
+	\Closure $form_content) {
+		return $this->output_remote_form_around_closure($options["url"],
+			$options, $form_content);
+	}
+
+	public function link_to_remote($text, $options = array(),
+	$html_options = array()) {
+		return $this->link_to_function($text, $this->remote_function($options),
+			$html_options);
+	}
+
+	public function link_to_function($text, $javascript, $options = array()) {
+		if (isset($options["onclick"]))
+			$options["onclick"] .= "; ";
+		else
+			$options["onclick"] = "";
+
+		$options["onclick"] .= $javascript . "; return false;";
+
+		if (!isset($options["href"]))
+			$options["href"] = "#";
+
+		return "<a href=\"" . $options["href"] . "\""
+			. HtmlHelper::options_for_link($options) . ">" . $text . "</a>";
+	}
+
+	public function options_for_ajax($options) {
+		$js_options = array();
+
+		$js_options["asynchronous"] = ($options["type"] != "synchronous");
+
+		if (isset($options["method"]))
+			$js_options["method"] = "'" . $options["method"] . "'";
+
+		if (isset($options["position"]))
+			$js_options["insertion"] = "'" . strtolower($options["position"])
+				. "'";
+
+		if (isset($options["script"]))
+			$js_options["evalScripts"] = true;
+
+		if (isset($options["form"]))
+			$js_options["parameters"] = "Form.serialize(this)";
+		elseif (isset($options["submit"]))
+			$js_options["parameters"] = "Form.serialize('" . $options["submit"]
+				. "')";
+		elseif (isset($options["with"]))
+			$js_options["parameters"] = $options["with"];
+
+		if (isset($js_options["parameters"]))
+			$js_options["parameters"] .= " + '&";
+		else
+			$js_options["parameters"] = "'";
+
+		$js_options["parameters"] .= "authenticity_token=' + "
+			. "encodeURIComponent('"
+			. $this->controller->form_authenticity_token() . "')";
+
+		return $this->options_for_javascript($js_options);
+	}
+
+	public function options_for_javascript($options) {
+		if (!array_keys($options))
+			return "{}";
+		else {
+			$t = array_map(function($k) use ($options) {
+				return $k . ":" . $options[$k];
+			}, array_keys($options));
+
+			sort($t);
+
+			return "{" . join(", ", $t) . "}";
+		}
+	}
+
+	/* the guts of remote_form_for() and form_remote_tag() */
+	protected function output_remote_form_around_closure($url_or_obj,
+	$options = array(), \Closure $form_content) {
+		$options["form"] = true;
+
+		if (!isset($options["html"]))
+			$options["html"] = array();
+
+		if (isset($options["html"]["onsubmit"]))
+			$options["html"]["onsubmit"] .= "; ";
+		else
+			$options["html"]["onsubmit"] = "";
+
+		$options["html"]["onsubmit"] .= $this->remote_function($options)
+			. "; return false;";
+
+		return $this->output_form_around_closure($url_or_obj, $options["html"],
+			$form_content);
+	}
+
+	public function remote_form_for($obj, $options = array(),
+	\Closure $form_content) {
+		if (!$obj)
+			throw new HalfMoonException("invalid object passed to "
+				. "remote_form_for()");
+
+		$this->form_object = $obj;
+
+		return $this->output_remote_form_around_closure($options["url"],
+			$options, $form_content);
+	}
+
+	public function remote_function($options) {
+		$javascript_options = $this->options_for_ajax($options);
+
+		$update = "";
+		if (isset($options["update"]) && is_array($options["update"])) {
+			$update = array();
+			if (isset($options["update"]["success"]))
+				array_push($update, "success:'" . $options["update"]["success"]
+					. "'");
+			if (isset($options["update"]["failure"]))
+				array_push($update, "failure:'" . $options["update"]["failure"]
+					. "'");
+			$update = "{" . join(",", $update) . "}";
+		}
+
+		elseif (isset($options["update"]))
+			$update = "'" . $options["update"] . "'";
+
+		$function = "new Ajax." . ($update == "" ? "Request("
+			: "Updater(" . $update . ", ");
+
+		$function .= "'" . HtmlHelper::link_from_obj_or_string($options["url"])
+			. "', " . $javascript_options . ")";
+
+		if (isset($options["before"]))
+			$function = $options["before"] . "; " . $function;
+
+		if (isset($options["after"]))
+			$function = $function . "; " . $options["after"];
+
+		if (isset($options["condition"]))
+			$function = "if (" . $options["condition"] . ") { " . $function
+				. "; }";
+
+		if (isset($options["confirm"]))
+			$function = "if (confirm('" . $options["confirm"] . "')) { "
+				. $function . "; }";
+
+		return $function;
+	}
+}
+
+?>
