jcs/halfmoon

commit 05a7cabd95232784ffce9424950380510a8e2794

controller: map default view directory properly from camel cased controller name

lib/controller.php
@@ -280,9 +280,13 @@ class ApplicationController {
 
 			/* if we have no directory, assume it's the in the current
 			 * controller's views */
-			if (!strpos($tf, "/"))
-				$tf = strtolower(preg_replace("/Controller$/", "",
-					Utils::current_controller_name())) . "/" . $tf;
+			if (!strpos($tf, "/")) {
+				/* AdminSomethingController -> admin_something */
+				$words = preg_split('/(?<=\\w)(?=[A-Z])/',
+					preg_replace("/Controller$/", "",
+					Utils::current_controller_name()));
+				$tf = strtolower(join("_", $words)) . "/" . $tf;
+			}
 
 			/* partial template files start with _ */
 			if (is_array($template) && isset($template["partial"]))
@@ -673,7 +677,7 @@ class ApplicationController {
 
 				return false;
 			}
-		
+
 			if (isset($this->redirected_to))
 				return false;
 		}

commit 0a06fc55ec0faa1e75fe3b0f939a33d5e8199e67

fix a regression from 05a7cabd: if path has non-root slash, it's relative to views/

lib/controller.php
@@ -281,10 +281,15 @@ class ApplicationController {
 			else
 				$tf = $template;
 
-			/* if we have no directory, assume it's the in the current
-			 * controller's views */
-			if (substr($tf, 0, 1) != "/")
-				/* AdminSomethingController -> admin_something */
+			if (substr($tf, 0, 1) == "/")
+				/* full path, just use it */
+				;
+			elseif (strpos($tf, "/") !== false)
+				/* path relative to base view path */
+				$tf = HALFMOON_ROOT . "/views/" . $tf;
+			else
+				/* just a file in this controller's directory
+				 * (AdminSomethingController -> admin_something) */
 				$tf = $this->view_template_path() . $tf;
 
 			/* partial template files start with _ */